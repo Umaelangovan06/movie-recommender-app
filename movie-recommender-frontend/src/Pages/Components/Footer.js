14084d008feb62dd21e3ad5a913cf06be5cb588d
Uma Elangovan <umaelangovan06@gmail.com>
2025-09-10 09:32:52 +0530

    Initial commit: Movie Recommendation Full stack project

---
+++ movie-recommender-frontend/src/Pages/Components/Footer.js
@@ -0,0 +1,39 @@
+// Footer Component
+import "./styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div id="row1" className="row ">
+                    <a
+                        href="https://www.linkedin.com/in/umaelangovan06"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                    <a
+                        href="https://github.com/Umaelangovan06"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/uma__elangovan"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+                <div id="row1" className="row">
+                    © Developed By Uma Elangovan
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
